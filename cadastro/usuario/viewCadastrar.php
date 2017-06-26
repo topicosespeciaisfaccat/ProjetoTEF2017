821ca84ed5489e6e4caede6a2a29552b36ae9531
mateus marcon <mateus marcon>
2017-06-26 20:44:25 -0300

    save arezzo files

--- cadastro/usuario/viewCadastrar.php
+++ cadastro/usuario/viewCadastrar.php
@@ -10,28 +10,36 @@
 	<?php require_once "./layout/header.php";?>
     <!-- FIM DE CABEÇALHO -->
 
+	<div class="conteudo">
+		<section>
+			<div class="home">	
+				<div class="wrapperadmin">
+
+					<h1>Formulario de cadastro de usuario</h1>
+
+					<form method="POST" action="index.php?r=cadastro/usuario&p=cadastrar">
+						<p>CPF:</p>
+						<p><input type="text" maxlength="11" minlength="11" name="txtCpf"></p>
+						<p>Nome do usuário:</p>
+						<p><input type="text" maxlength="200" name="txtNome"></p>
+						<p>E-mail:</p>
+						<p><input type="text" maxlength="200" name="txtEmail"></p>
+						<p>Senha:</p>
+						<p><input type="password" maxlength="20" name="txtSenha"></p>
+						<p>Tipo:</p>
+						<p><input type="text" name="txtTipo" value="1"></p>
 
 
-	<h1>Formulario de cadastro de usuario</h1>
+						<input type="submit" name="Cadastrar usuario">
+						<input type="hidden" name="formularioCadastroUsuario">
+					</form>
+			</div>
+		</div>
+	</section>
+</div>
 
-	<form method="POST" action="index.php?r=cadastro/usuario&p=cadastrar">
-		<p>Cpf:</p>
-		<p><input type="text" maxlength="11" minlength="11" name="txtCpf"></p>
-		<p>Nome do usuario :</p>
-		<p><input type="text" maxlength="200" name="txtNome"></p>
-		<p>Email :</p>
-		<p><input type="text" maxlength="200" name="txtEmail"></p>
-		<p>Senha :</p>
-		<p><input type="password" maxlength="20" name="txtSenha"></p>
-		<p>Tipo :</p>
-		<p><input type="text" name="txtTipo" value="1"></p>
-
-
-		<input type="submit" name="Cadastrar usuario">
-		<input type="hidden" name="formularioCadastroUsuario">
-	</form>
 	<!-- INICIO DE RODAPÉ -->
-	   <?php require_once "/layout/footer.php";?>
+	<?php require_once "./layout/footer.php";?>
    <!-- INICIO DE RODAPÉ <--></-->
 </body>
 </html>
