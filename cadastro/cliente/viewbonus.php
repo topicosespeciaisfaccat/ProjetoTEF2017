b72a34a7bdc3696f364f166cd9a6bfe3c3f4fee3
mateusmarcon <mateus-marcon@hotmail.com>
2017-06-30 14:43:15 -0300

    tela cliente
    criado HTML e pasta

---
+++ cadastro/cliente/viewbonus.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  	<meta charset="utf-8">
+	<title><?=$titulo?></title>
+	<link rel="stylesheet" type="text/css" href="./layout/css/estilo.css">
+<style>
+
+ </style>
+
+</head>
+<body class="center clearfix">
+    <!-- INICIO DE CABEÇALHO -->
+		<?php include "./layout/header.php"; ?>
+    <!-- FIM DE CABEÇALHO -->
+
+	<!-- INICIO DE CORPO DO SITE -->
+	<div class="conteudo">
+		<section>
+			<div class="home">
+				<div class="wrapper">
+						
+								<h1> Bem vindo usuário!</h1>
+								<h2> Você possuí x bônus</h2>
+						
+				</div>	
+			</div>
+		</section>
+	</div>
+    <!-- FIM DE CORPO DO SITE -->
+
+
+ 	<!-- INICIO DE RODAPÉ -->
+		<?php include "./layout/footer.php";?>
+   <!-- INICIO DE RODAPÉ -->
+</body>
+</html>
